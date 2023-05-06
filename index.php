52911dbb197796e80597e07b1733612e52127808
Muela <joaquin1matiasmail.com>
2023-05-06 00:28:23 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>A1</title>
+</head>
+<body class="contenido container">
+    <div class="container">
+        <h1 class="title">Gato lindo</h1>
+    </div>    
+
+    <div class="card-group">
+        <div class="card">
+          <img src="/img/gato1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">gato1</h5>
+            <p class="card-text">gato lindo</p>
+          </div>
+        </div>
+        <div class="card">
+          <img src="/img/gato1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">gato1</h5>
+            <p class="card-text">gato lindo</p>
+          </div>
+        </div>
+        <div class="card">
+          <img src="/img/gato1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">gato1</h5>
+            <p class="card-text">gato lindo</p>
+          </div>
+        </div>
+      </div>
+    
+
+</body>
+</html>
